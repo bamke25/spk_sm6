8f0dcd31aadd0fdb801d9dcc825b7632ef673c85
bamke25 <nurullahke25@gmail.com>
2021-07-22 00:55:23 +0700

    Update DataChart.php

--- application/controllers/DataChart.php
+++ application/controllers/DataChart.php
@@ -6,6 +6,7 @@ class DataChart extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->model('Chart_Model');
+		$this->load->library('form_validation');
 	}
 
 	function index()
